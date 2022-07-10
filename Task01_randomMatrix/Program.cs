385321ed207af79e82fd85162bb838f5f391b8ad
Alexander <shagov25@gmail.com>
2022-07-10 17:25:21 +0300

    Добавьте файлы проекта.

---
+++ Task01_randomMatrix/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task01_randomMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
